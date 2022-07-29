d52dc3b55b3a6e3176ee27236be4ab49ffab9322
ddboy19912 <99345157+ddboy19912@users.noreply.github.com>
2022-07-29 17:59:25 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Posts } from '../typings'
 
-const Home: NextPage = () => {
+
+interface Props{ 
+  posts: [Posts]
+}
+
+const Home: NextPage = ({posts}: Props) => {
+
+  
+
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -21,10 +32,43 @@ const Home: NextPage = () => {
 <div className="hidden md:inline-flex h-32 lg:h-full ">
   <img src="https://www.nicepng.com/png/full/48-483810_medium-logo-png.png" alt="" />
 </div>
-
+{posts.map((post) => {
+  <Link key={post._id} href={`/post/${post.slug.current}`} >
+  <div>
+    <img src={urlFor(post.mainImage).url()!} alt="" />
+  </div>
+  </Link>
+})}
 </div>
     </div>
   )
+}
+
+
+export const getServerSideProps = async () => {
+  const query = await `
+  *[_type == "post"]{
+  _id,
+  title,
+  author -> {
+  name,
+  image,
+},
+description,
+slug,
+mainImage
+
+}
+  `;
+
+  const posts = await sanityClient.fetch(query);
+
+return{
+    props: {
+      posts,
+    }
+  }
+
 }
 
 export default Home
